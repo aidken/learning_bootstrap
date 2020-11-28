ec066f68b7944b882e7c214a03995f0f6da8508e
Ken Aida <aidken+github@gmail.com>
2020-11-28 10:23:12 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1 class="jumbotron text-center" style="margin-bottom:0">Hello Vanilla!</h1>
+
+  <div class="container" style="margin-top:20px">
+
+    <div class="row">
+      <div class="col-sm-4">
+        <p>We use the same configuration as Parcel to bundle this sandbox, you can find more
+        info about Parcel 
+        <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.</p>
+      </div>
+
+      <div class="col-sm-8">
+        <p>This is my learning of <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
+      </div>
+
+    </div>
+    
+  </div>
+`;
