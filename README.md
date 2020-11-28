bd884072f0d932ae55eb4c13c1610c9baea355eb
Ken Aida <aidken+github@gmail.com>
2020-11-28 13:07:32 +0900

    Adding text.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# learning_bootstrap
-Created with CodeSandbox
+# Learning [Bootstrap](https://getbootstrap.com/)
+
+Learning how to apply [Bootstrap](https://getbootstrap.com/) to my website.
+
+Looks like Bootstrap is a bunch of CSS you can use for your website. Just make your elements
+class of some Bootstrap classes and CSS gets applied.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1 class="jumbotron text-center" style="margin-bottom:0">Hello Vanilla!</h1>
+<h1 class="jumbotron text-center" style="margin-bottom:0">Learning Bootstrap 4</h1>
 
   <div class="container" style="margin-top:20px">
 
@@ -13,7 +13,12 @@ document.getElementById("app").innerHTML = `
       </div>
 
       <div class="col-sm-8">
-        <p>This is my learning of <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
+        <h3>Putting Bootstrap to work.</h3>
+        <h5>November 27th, 2020</h5>
+        <p>This is my learning of <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>.</p>
+        <p>Bootstrap seems to be a bunch of CSS. When you make your elements of your webpage
+        a class of Bootstrap classes, then Bootstrap styles are applied. Not difficult.</p>
+        <p>How can I add colors?</p>
       </div>
 
     </div>
